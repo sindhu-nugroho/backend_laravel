d3de42433d1c39281954455de60f50ed6ed7e233
sindhu-nugroho <sindhuwijaya412@gmail.com>
2026-02-02 09:05:03 +0700

    simple scheduler

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,13 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Log::info('Scheduler Laravel 12 jalan tiap menit');
+})->everyMinute();
